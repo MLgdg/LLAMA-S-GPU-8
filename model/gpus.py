6b135e149ec704f7b675984dd347033e8910e0fd
MLgdg <986285044@qq.com>
2023-04-21 21:17:22 +0800

    Update gpus.py

--- model/gpus.py
+++ model/gpus.py
@@ -7,16 +7,9 @@ from text_embedding import TextEmbeddings
 
 GPU = [0,1,2,3,4,5,6,7]
 
+class GPUs(nn.Module):
+	def __init__(self, model, gpu_list):
+		super().__init__()
 
-class LLAMA(nn.Module):
-    def __init__(self, config, model_embeddings_weights)
-        super().__init__()
-        self.E = TextEmbeddings(config)
-        self.T = Transformer(config.hidden_size, config.num_hidden_layers, config.num_attention_heads)
-        self.H = LLAMAHead(self.E.word_embeddings.weight)
-        self.H.set_embeddings_weights(self.E.word_embeddings.weight)
-    def forward(self, token):
-        out = self.E(token)
-        out = self.T(out)
-        out = self.H(out)
-        return out, out[:, -1, :]
+	
+
