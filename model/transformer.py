bdc50ee79270d250b0c9d8a2a113242a4bb47f99
MLgdg <986285044@qq.com>
2023-05-08 21:45:28 +0800

    qwe

--- model/transformer.py
+++ model/transformer.py
@@ -65,5 +65,29 @@ class LLAMAHead(nn.Module):
         return lm_logits
 
 
+class LLAMA(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.E = TextEmbeddings(config)
+        print('model embedding done')
+        #print(config.num_hidden_layers)
+        self.T = Transformer(config.hidden_size, config.num_hidden_layers, config.num_attention_heads)
+        #self.T = GPUs(T, config.gpulsit)
+        print('model Transformer done')
+        H = LLAMAHead(self.E.word_embeddings.weight)
+        H.set_embeddings_weights(self.E.word_embeddings.weight)
+        print('model head done')
+        self.H = H#.cuda(config.gpulsit[-1])
+    def forward(self, token):
+
+        out, pad_mask, causal_mask= self.E(token)
+        #print("embed shape", out.shape)
+        #out = out.permute(1, 0, 2)  # NLD -> LND
+        out = self.T(out, pad_mask, causal_mask)
+        #out = out.permute(1, 0, 2)
+
+        out = self.H(out)
+        return out, out[:, -1, :]
 
 
